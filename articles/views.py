1e3507cd581ae11043700d1d5baf735015eea8e6
amin <seyedamin963@gmail.com>
2021-07-21 15:51:26 +0430

    Just a logged in user can create a new post, and each user just can edit and delete self post

--- articles/views.py
+++ articles/views.py
@@ -1,30 +1,44 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Article
 # Create your views here.
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
 
-class ArticleDetailView(DeleteView):
+class ArticleDetailView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = 'article_detail.html'
 
-class ArticleEditView(UpdateView):
+class ArticleEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'body')
     template_name = 'article_edit.html'
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
     
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
     
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author',)
-    
+    # fields = ('title', 'body', 'author',)
+    fields = ('title', 'body',)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(form_vali(form))
+            
